d32b61ca762ada65769365ea958462e35ef9e0a5
Tegarr123 <fadhino987@gmail.com>
2025-06-03 18:19:20 +0700

    add navbar

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,92 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+      debugShowCheckedModeBanner: false,
+      home: SplitInApp(),
+    );
+  }
+}
+
+class SplitInApp extends StatefulWidget {
+  const SplitInApp({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _SplitInState();
+}
+
+class _SplitInState extends State<SplitInApp> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() => _selectedIndex = index);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+
+    return Scaffold(
+      appBar: AppBar(title: Text("Ini adalah Judul")),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: Container(
+        height: 70,
+        width: 70,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle
+        ),
+        child: FloatingActionButton(
+          onPressed: () {},
+          elevation: 20,
+          
+          child: ImageIcon(AssetImage("assets/images/star.png"), size: 50,color: const Color.fromARGB(255, 244, 157, 26),),
+
+          shape: CircleBorder(),
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(30),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black12,
+                blurRadius: 12,
+                offset: Offset(0, 8),
+              ),
+            ],
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(30),
+            child: BottomNavigationBar(
+              type: BottomNavigationBarType.fixed,
+              backgroundColor: Colors.white,
+              currentIndex: _selectedIndex,
+              selectedItemColor: Colors.blue,
+              unselectedItemColor: Colors.grey,
+              showSelectedLabels: true,
+              showUnselectedLabels: false,
+              onTap: _onItemTapped,
+              mouseCursor: MouseCursor.uncontrolled,
+              iconSize: 25,
+              items: const [
+                BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.group),
+                  label: 'Groups',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.notifications),
+                  label: 'Notifications',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.person),
+                  label: 'Profile',
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
