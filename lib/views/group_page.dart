c5474b196dbfde38a847faa5ca842823a15ef168
ishaqyudha01@gmail.com <ishaqyudha01@gmail.com>
2025-06-08 19:17:46 +0700

    add: group page

--- lib/views/group_page.dart
+++ lib/views/group_page.dart
@@ -3,26 +3,56 @@ import 'package:provider/provider.dart';
 import 'package:splitin_frontend/models/provider.dart';
 import 'package:splitin_frontend/widgets/bottom_navigation_bar.dart';
 import 'package:splitin_frontend/widgets/floating_action_button.dart';
+import 'package:splitin_frontend/widgets/group_card.dart';
 
 class GroupPage extends StatefulWidget {
   const GroupPage({super.key});
 
   @override
-  State<StatefulWidget> createState() => _GroupPageState();
+  State<GroupPage> createState() => _GroupPageState();
 }
 
 class _GroupPageState extends State<GroupPage> {
+  final List<Map<String, dynamic>> groupData = [
+    {
+      'title': 'Trip to Bali',
+      'image': 'assets/images/person.jpeg',
+      'members': [
+        'assets/images/person.jpeg',
+        'assets/images/person.jpeg',
+        'assets/images/person.jpeg'
+      ]
+    },
+    {
+      'title': 'Office Lunch',
+      'image': 'assets/images/person.jpeg',
+      'members': [
+        'assets/images/person.jpeg',
+        'assets/images/person.jpeg'
+      ]
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Consumer<ProviderModel>(
       builder: (context, value, child) => Scaffold(
         appBar: AppBar(
-          title: Text(
-            "Groups",
-            style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+          automaticallyImplyLeading: false,
+          title: Align(
+            alignment: Alignment.centerLeft,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20.0),
+              child: const Text(
+                "Groups",
+                style: TextStyle(
+                  color: Color(0xFF388E3C),
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                ),
+              ),
+            ),
           ),
-          backgroundColor: const Color.fromARGB(255, 155, 207, 249),
         ),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
         floatingActionButton: SizedBox(
@@ -32,10 +62,39 @@ class _GroupPageState extends State<GroupPage> {
             fit: BoxFit.fill,
             child: SplitinFloatingActionButton(),
           ),
-        ), // edit ini
-        body: Container(
-          color: const Color.fromARGB(255, 155, 207, 249),
-        ), // edit ini
+        ),
+        body: Stack(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 80),
+              child: ListView.builder(
+                itemCount: groupData.length,
+                itemBuilder: (context, index) {
+                  final group = groupData[index];
+                  return GroupCard(
+                    title: group['title'],
+                    imageUrl: group['image'],
+                    memberImages: List<String>.from(group['members']),
+                    onTap: () {
+                      // Implement navigation to group detail
+                    },
+                  );
+                },
+              ),
+            ),
+            Positioned(
+              bottom: 30,
+              right: 20,
+              child: FloatingActionButton(
+                onPressed: () {
+                  // Add new group action
+                },
+                backgroundColor: Colors.orange,
+                child: const Icon(Icons.add, size: 32),
+              ),
+            ),
+          ],
+        ),
         bottomNavigationBar: SplitInNavBar(),
       ),
     );
--- lib/widgets/floating_action_button.dart
+++ lib/widgets/floating_action_button.dart
@@ -4,14 +4,21 @@ import 'package:splitin_frontend/models/provider.dart';
 
 class SplitinFloatingActionButton extends Consumer<ProviderModel> {
   SplitinFloatingActionButton()
-    : super(builder: (context, value, child) => FloatingActionButton(
-        onPressed: () {
-          Navigator.pushNamed(context, "/bill-item");
-        },
-        shape: CircleBorder(),
-        elevation: 12,
-        backgroundColor: const Color.fromARGB(255, 249, 181, 204),
-        child: ImageIcon(AssetImage("assets/images/star.png"), size: 50, color: const Color.fromARGB(255, 242, 114, 2),)
-      
-    ),);
+      : super(
+    builder: (context, value, child) => FloatingActionButton(
+      heroTag: 'splitin-fab',
+      onPressed: () {
+        Navigator.pushNamed(context, "/bill-item");
+      },
+      shape: CircleBorder(),
+      elevation: 12,
+      backgroundColor: const Color.fromARGB(255, 249, 181, 204),
+      child: ImageIcon(
+        AssetImage("assets/images/star.png"),
+        size: 50,
+        color: const Color.fromARGB(255, 242, 114, 2),
+      ),
+    ),
+  );
 }
+
